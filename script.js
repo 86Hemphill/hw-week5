09d5322cb573024205cf090bffb890622f7bf5f0
Logan Hemphill <logan.hemphill86@gmail.com>
2020-04-04 00:59:03 -0400

    Building .js

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
 // Create Variables
-var currentJumbo = $("#currentDay");
+var currentJumbo = $('#currentDay');
+var currentDayTime = parseInt(moment().format('H'));
+var startTime = 4;
+var endTime = 10;
 
-var currentDayTime = moment();
 
 // Append current day and time to Jumbotron
-$("#currentDay").text(currentDayTime.format("LLLL"));
+$('#currentDay').text(moment().format('LLLL'));
 
 // Create Rows
-for (i = 0; i < 24; i++){
+for (var i = 0; i < 19; i++){
 
-    $("#container").append('<div id="div-row" class="row"></div>')
-
-
+    var divRow = $('<div class="row">');
+    divRow.attr('data-id', i);
+    $('.container').append(divRow);
+    // $('.container').append(`
+    //     <div    id='div-row' 
+    //             class='row'
+    //             data-id='i'>`); 
+        
 // Create Grid - Time
-    $("#div-row").append(`<div 
-        id="hourCol" 
-        class="col-lg-2"></div>`);
-
-
+    var timeCol = $('<div class="time-block hour col-2">');
+    var hour = moment(startTime, 'h').format('LT');
+    timeCol.text(hour);
+    divRow.append(timeCol);
+    startTime++
+    // $("#div-row").append(`<div 
+    //     id='timeCol' 
+    //     class='col-lg-2 time-block hour'
+    //     ></div>`);
 
 // Create Grid - Event
-    $("#div-row").append(`<div 
-        id="eventDescCol" 
-        class="col-lg-10"></div>`);
+    var eventDescCol = $('<div class="description col-8">');
+    eventDescCol.attr('data-time', startTime);
 
+    if (moment(startTime).isBefore(currentDayTime)) {
+        eventDescCol.addClass('past');
+        console.log('past');
+    } else if (moment(startTime).isSame(currentDayTime)) {
+        eventDescCol.addClass('present');
+        console.log('present');
+    } else if (moment(startTime).isAfter(currentDayTime)) {
+        eventDescCol.addClass('future');
+        console.log('future');
+    };
 
+    var eventDescText = $('<textarea class="col-12" cols="90" rows="2"/>');
+    eventDescText.attr('data-id', i);
+    eventDescCol.html(eventDescText);
+    divRow.append(eventDescCol);
 
 // Create Grid - Save
-    $("#div-row").append(`<div 
-        id="saveCol" 
-        class="col-lg-2"></div>`);
 
+    var saveCol = $('<div class="saveBtn col-2">');
+    divRow.append(saveCol);
+    var saveBtn = $('<i class="fas fa-save"></i>');
+    saveBtn.attr('data-id', i);
+    saveCol.html(saveBtn);
+    
 
 };
 
